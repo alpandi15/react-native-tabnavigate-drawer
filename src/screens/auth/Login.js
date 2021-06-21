188c3c0845c312c99e7054a368755523c7d0a7db
Muhammad Al-Pandi <m.alpandi57@gmail.com>
2021-06-21 14:01:41 +0700

    Update login

--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, Button} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
 const Login = ({navigation}) => {
   return (
-    <View style={styles.center}>
-      <Text>LOGIN</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('ForgotPassword')}>
-        <Text>Register</Text>
-      </TouchableOpacity>
+    <View style={styles.wrapper}>
+      <View>
+        <Text>Masuk ke Akun Stand Kamu</Text>
+        <Text>
+          Gunakan akun stand yang telah di daftarkan oleh admin foodcourt
+        </Text>
+      </View>
+      <View style={styles.center}>
+        <Text>LOGIN</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'red',
+          padding: 10,
+          bottom: 0,
+          position: 'absolute',
+          width: '100%',
+          flex: 1,
+          alignItems: 'center'
+        }}>
+        <Button title="Masuk" style={{ width: '100%' }}/>
+        <TouchableOpacity onPress={() => navigation.navigate('ForgotPassword')}>
+          <Text>Lupa Password</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    position: 'relative',
+  },
   center: {
     flex: 1,
     justifyContent: 'center',
